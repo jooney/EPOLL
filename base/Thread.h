7b3efe17563aad2ba572717581b3c3c535a4492c
jooney <vikingpanzer@126.com>
2017-09-26 15:25:40 +0800

    base/Thread.cpp

---
+++ base/Thread.h
@@ -0,0 +1,38 @@
+/*
+ * Thread.h
+ *
+ *  Created on: 2017Äê9ÔÂ22ÈÕ
+ *      Author: jjz
+ */
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+
+#include <functional>
+#include <memory>
+#include <pthread.h>
+class Thread
+{
+public:
+	typedef std::function<void()> ThreadFunc;
+	Thread(const ThreadFunc&,const std::string& name);
+	~Thread();
+	void start();
+	int join();
+	bool started() const {return _started;}
+
+	const std::string& name() const {return _name;}
+private:
+	bool         _started;
+	bool         _joined;
+	pthread_t    _tid;              //thread id
+	ThreadFunc   _func;
+	std::string  _name;
+
+};
+
+
+
+
+#endif /* THREAD_H_ */
